fc273e24e9043eba8d29b779ff041a7cbe6c998e
Sean Sundberg <seansund@us.ibm.com>
2022-09-13 21:38:22 -0500

    Adds support for common tags across resources
    - Adds common_tags variable set to global scope in the metadata
    - Concatenates common_tags and tags values when applied to object storage instance
    - Updates resource_group dependency reference to new location
    
    closes #38
    
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- test/stages/stage2-object-storage.tf
+++ test/stages/stage2-object-storage.tf
@@ -5,4 +5,6 @@ module "dev_object-storage" {
   resource_group_name = module.resource_group.name
   name_prefix = var.name_prefix
   ibmcloud_api_key = var.ibmcloud_api_key
+  common_tags          = ["common", "test"]
+  tags                 = ["test"]
 }
