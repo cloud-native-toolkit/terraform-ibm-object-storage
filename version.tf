f82be20c0edffc8530113e1ba8155ce462a44955
Daniel Butler <dannyeb@gmail.com>
2022-12-05 17:07:36 +0000

    feat: migrate to common automation
    Signed-off-by: Daniel Butler <dannyeb@gmail.com>

--- version.tf
+++ version.tf
@@ -3,8 +3,16 @@ terraform {
 
   required_providers {
     ibm = {
-      source = "ibm-cloud/ibm"
+      source  = "ibm-cloud/ibm"
       version = ">= 1.18.0"
+    }
+    null = {
+      source  = "hashicorp/null"
+      version = ">=3.2.1"
+    }
+    external = {
+      source  = "hashicorp/external"
+      version = ">=2.2.3"
     }
   }
 }
